718c1a9eb345470ef99d08d0a3a7bf9b6cbe2b70
54x13 <71689303+54x13@users.noreply.github.com>
2020-10-22 23:59:42 +0800

    未完成的下半部

--- main.ts
+++ main.ts
@@ -2,7 +2,13 @@ basic.forever(function () {
     for (let x = 0; x <= 4; x++) {
         for (let y = 0; y <= x; y++) {
             led.plot(4 - x + y, y)
-            basic.pause(100)
+            basic.pause(200)
+        }
+    }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.plot(1 - x + (1 + y), y + 1)
+            basic.pause(200)
         }
     }
 })
